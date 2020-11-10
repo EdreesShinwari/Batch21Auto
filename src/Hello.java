8b0e27e913b9a74c7cdc720fc3101081e2a9da42
EdreesShinwari <ekshinwari@gmail.com>
2020-11-10 12:54:35 -0500

    Fixed mac and windows commit dialog issue

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,10 @@
 public class Hello {
     // more stuff
     // Mac users and windows users are happy to see commit dialog finally
+
+    // I have already published on GitHub.com
+    // now I am ready to make more commits
+    // but do I need to share again?
+    // NO way
+    // just push!!
 }
